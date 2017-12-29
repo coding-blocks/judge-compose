a5e584fe1a1e4cfc63b9fdd4ad4f1d516848fc09
Arnav Gupta <arnav@codingblocks.com>
2017-12-29 17:57:18 +0530

    log everything
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- test_docker/test/judge-api_run_cpp.spec.ts
+++ test_docker/test/judge-api_run_cpp.spec.ts
@@ -2,12 +2,15 @@ import * as request from 'request'
 import {expect} from 'chai'
 import chai = require('chai')
 import chaiString = require('chai-string')
+import * as debug from 'debug'
+
+const log = debug('test:api:langs')
 chai.use(chaiString)
 
 describe('judge-api:/api/runs [Language: C++]', () => {
 
-    it('POST: Correct submission works', (done) => {
-        let source = `
+  it('POST: Correct submission works', (done) => {
+    let source = `
 #include <iostream>
 using namespace std;
 int main () {
@@ -17,40 +20,42 @@ int main () {
     return 0;
 }
 `
-        let stdin = 'World'
+    let stdin = 'World'
 
-        request.post(`http://${process.env.JUDGEAPI_HOST}:${process.env.JUDGEAPI_PORT}/api/runs`,
-            {
-                json: {
-                    source: (new Buffer(source).toString('base64')),
-                    lang: 'cpp',
-                    stdin: (new Buffer(stdin).toString('base64'))
-                }
-            },
-            (err, resp, body) => {
-                expect(body.stderr).to.eq('')
-                let stdout = (new Buffer(body.stdout, 'base64')).toString()
-                expect(stdout).to.eq('Hello World')
-                done()
-            })
-    })
-    it('POST: Compilation error is in stderr', (done) => {
-        let source = 'SOME GARBAGE THAT IS NOT VALID CPP SOURCE'
-        let stdin = 'World'
+    request.post(`http://${process.env.JUDGEAPI_HOST}:${process.env.JUDGEAPI_PORT}/api/runs`,
+      {
+        json: {
+          source: (new Buffer(source).toString('base64')),
+          lang: 'cpp',
+          stdin: (new Buffer(stdin).toString('base64'))
+        }
+      },
+      (err, resp, body) => {
+        log(body)
+        expect(body.stderr).to.eq('')
+        let stdout = (new Buffer(body.stdout, 'base64')).toString()
+        expect(stdout).to.eq('Hello World')
+        done()
+      })
+  })
+  it('POST: Compilation error is in stderr', (done) => {
+    let source = 'SOME GARBAGE THAT IS NOT VALID CPP SOURCE'
+    let stdin = 'World'
 
-        request.post(`http://${process.env.JUDGEAPI_HOST}:${process.env.JUDGEAPI_PORT}/api/runs`,
-            {
-                json: {
-                    source: (new Buffer(source).toString('base64')),
-                    lang: 'cpp',
-                    stdin: (new Buffer(stdin).toString('base64'))
-                }
-            },
-            (err, resp, body) => {
-                expect(body.stdout).to.eq('')
-                let stderr = (new Buffer(body.stderr, 'base64')).toString()
-                expect(stderr).to.startWith('source.cpp:1:1: error')
-                done()
-            })
-    })
+    request.post(`http://${process.env.JUDGEAPI_HOST}:${process.env.JUDGEAPI_PORT}/api/runs`,
+      {
+        json: {
+          source: (new Buffer(source).toString('base64')),
+          lang: 'cpp',
+          stdin: (new Buffer(stdin).toString('base64'))
+        }
+      },
+      (err, resp, body) => {
+        log(body)
+        expect(body.stdout).to.eq('')
+        let stderr = (new Buffer(body.stderr, 'base64')).toString()
+        expect(stderr).to.startWith('source.cpp:1:1: error')
+        done()
+      })
+  })
 })
